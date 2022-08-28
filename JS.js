152a0d2a4d3a50fca12c48e9af3b256346140999
Fusse18 <nspinettojung@gmail.com>
2022-08-28 17:14:53 -0300

    algunos arreglos

--- JS.js
+++ JS.js
@@ -8,3 +8,15 @@ setInterval(() => {
     numero.innerHTML = counter + "%";
   }
 },20);
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.backgroundColor = "rgb(51, 51, 51)";
+  } else {
+    document.getElementById("nav").style.backgroundColor = "rgba(221, 221, 221, 0)";
+  }
+  prevScrollpos = currentScrollPos;
+} 
